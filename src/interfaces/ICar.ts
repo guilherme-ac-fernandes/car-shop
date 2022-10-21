690ede7060f8feeef89a2065f949ca917f6aae87
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-10-21 17:08:51 -0300

    refactor: refatoração das interfaces de vehicle e car

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,21 +1,27 @@
 import { z } from 'zod';
 import { VehicleZodSchema } from './IVehicle';
 
-export const CarZodSchema = VehicleZodSchema.extend({
-  doorsQty: z.number({
-    required_error: 'doorsQty is required',
-    invalid_type_error: 'doorsQty must be a number',
-  }).int({ message: 'Must be a integer' })
+const CarZodSchema = VehicleZodSchema.extend({
+  doorsQty: z
+    .number({
+      required_error: 'doorsQty is required',
+      invalid_type_error: 'doorsQty must be a number',
+    })
+    .int({ message: 'Must be a integer' })
     .positive({ message: 'Must be a positiver number' })
     .lte(4, { message: 'doorsQty must be less than 4' })
     .gte(2, { message: 'doorsQty must be greater than 2' }),
-  seatsQty: z.number({
-    required_error: 'seatsQty is required',
-    invalid_type_error: 'seatsQty must be a number',
-  }).int({ message: 'Must be a integer' })
+  seatsQty: z
+    .number({
+      required_error: 'seatsQty is required',
+      invalid_type_error: 'seatsQty must be a number',
+    })
+    .int({ message: 'Must be a integer' })
     .positive({ message: 'Must be a positiver number' })
     .lte(7, { message: 'seatsQty must be less than 7' })
     .gte(2, { message: 'seatsQty must be greater than 2' }),
 });
 
-export type ICar = z.infer<typeof CarZodSchema>;
+ type ICar = z.infer<typeof CarZodSchema>;
+
+export { CarZodSchema, ICar };
