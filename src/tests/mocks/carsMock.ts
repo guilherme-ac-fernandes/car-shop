8896adcfaaf38fa1ddef5f1d444502f9a62524e8
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-10-21 18:58:43 -0300

    feat: criação dos testes do service para rota /cars

--- src/tests/mocks/carsMock.ts
+++ src/tests/mocks/carsMock.ts
@@ -31,6 +31,16 @@ const carsMockForUpdate: ICar = {
   seatsQty: 5,
 };
 
+const carsMockForUpdateWrong: unknown = {
+  model: 'Palio',
+  year: '2011',
+  color: 'Preto',
+  status: true,
+  buyValue: 1000,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
 const carsMockWithIdUpdated: ICar & { _id: string } = {
   _id: '62cf1fc6498565d94eba52cd',
   model: 'Palio',
@@ -42,4 +52,10 @@ const carsMockWithIdUpdated: ICar & { _id: string } = {
   seatsQty: 5,
 };
 
-export { carsMock, carsMockWithId, carsMockForUpdate, carsMockWithIdUpdated };
+export {
+  carsMock,
+  carsMockWithId,
+  carsMockForUpdate,
+  carsMockWithIdUpdated,
+  carsMockForUpdateWrong,
+};
---
+++ src/tests/unit/services/cars.service.test.ts
@@ -0,0 +1,126 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import { ZodError } from 'zod';
+import { ErrorTypes } from '../../../errors/catalog';
+import CarModel from '../../../models/CarModel';
+import CarService from '../../../services/CarService';
+import {
+  carsMock,
+  carsMockWithId,
+  carsMockForUpdate,
+  carsMockWithIdUpdated,
+	carsMockForUpdateWrong,
+} from '../../mocks/carsMock';
+
+describe('Frame Service', () => {
+	const carModel = new CarModel();
+	const carService = new CarService(carModel);
+
+	before(() => {
+		sinon.stub(carModel, 'create').resolves(carsMockWithId);
+		sinon.stub(carModel, 'readOne')
+			.onCall(0).resolves(carsMockWithId) 
+			.onCall(1).resolves(null);
+		sinon.stub(carModel, 'read').resolves([carsMockWithId]);
+		sinon.stub(carModel, 'delete')
+			.onCall(0).resolves(carsMockWithId) 
+			.onCall(1).resolves(null);
+		sinon.stub(carModel, 'update')
+			.onCall(0).resolves(carsMockWithIdUpdated)
+			.onCall(1).resolves(null) 
+			.onCall(2).resolves(null);
+	});
+
+	after(() => sinon.restore());
+
+	describe('Create Car', () => {
+		it('Success', async () => {
+			const carCreated = await carService.create(carsMock);
+			expect(carCreated).to.be.deep.equal(carsMockWithId);
+		});
+
+		it('Failure', async () => {
+			let error;
+			try {
+				await carService.create({});
+			} catch (err) {
+				error = err
+			}
+			expect(error).to.be.instanceOf(ZodError);
+		});
+	});
+
+	describe('ReadOne Car', () => {
+		it('Success', async () => {
+			const carCreated = await carService.readOne(carsMockWithId._id);
+			expect(carCreated).to.be.deep.equal(carsMockWithId);
+		});
+
+		it('Failure', async () => {
+      let error;
+			try {
+				await carService.readOne(carsMockWithId._id);
+			} catch (err:any) {
+				error = err
+			}
+			expect(error, 'error should be defined').not.to.be.undefined;
+			expect(error.message).to.be.deep.equal(ErrorTypes.EntityNotFound);
+		});
+	});
+
+	describe('Read Cars', () => {
+		it('Success', async () => {
+			const carsArray = await carService.read();
+			expect(carsArray).to.be.deep.equal([carsMockWithId]);
+		});
+	});
+
+	describe('Delete Car', () => {
+		it('Success', async () => {
+			const carDelete = await carService.delete(carsMockWithId._id);
+			expect(carDelete).to.be.deep.equal(carsMockWithId);
+		});
+
+		it('Failure', async () => {
+      let error;
+			try {
+				await carService.delete(carsMockWithId._id);
+			} catch (err:any) {
+				error = err
+			}
+			expect(error, 'error should be defined').not.to.be.undefined;
+			expect(error.message).to.be.deep.equal(ErrorTypes.EntityNotFound);
+		});
+	});
+
+	describe('Update Car', () => {
+		it('Success', async () => {
+			const frameArray = await carService.update(carsMockWithId._id, carsMockForUpdate);
+			expect(frameArray).to.be.deep.equal(carsMockWithIdUpdated);
+		});
+
+		it('Failure: id invalid', async () => {
+      let error;
+			try {
+				await carService.update('123ERRADO', carsMockForUpdate);
+			} catch (err:any) {
+				error = err
+			}
+			
+			expect(error.message).to.be.equal(ErrorTypes.EntityNotFound);
+		});
+
+		it('Failure: car invalid', async () => {
+      let error;
+			try {
+				await carService.update(carsMockWithId._id, carsMockForUpdateWrong);
+			} catch (err:any) {
+				error = err
+			}
+		
+			expect(error).to.be.instanceOf(ZodError);
+		});
+
+		
+	});
+});
