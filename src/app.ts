99dc8b91fe8ef1ba43fa6b165ad7231e645bd921
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-10-21 19:22:38 -0300

    feat: implementa a rota /motorcycles a partir da /cars

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import 'express-async-errors';
 import errorHandler from './middlewares/error';
 import carRouter from './routes/CarRouter';
+import motorcycleRouter from './routes/MotorcycleRouter';
 
 const app = express();
 
 app.use(express.json());
 app.use('/cars', carRouter);
+app.use('/motorcycles', motorcycleRouter);
 app.use(errorHandler);
 
 export default app;
---
+++ src/models/MotorcycleModel.ts
@@ -0,0 +1,23 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { IMotorcycle } from '../interfaces/IMotorcycle';
+import MongoModel from './MongoModel';
+
+// A remoção do campo "__v": 0 pode ser removida com o versionKey: false, solução proposta
+// pelo colega de turma Rafael Moraes (Turma 20 B)
+const motorcycleMongooseSchema = new Schema<IMotorcycle>({
+  model: String,
+  year: Number,
+  color: String,
+  status: { type: Boolean, required: false },
+  buyValue: Number,
+  category: String,
+  engineCapacity: Number,
+}, { versionKey: false });
+
+class MotorcycleModel extends MongoModel<IMotorcycle> {
+  constructor(model = mongooseCreateModel('Motorcycle', motorcycleMongooseSchema)) {
+    super(model);
+  }
+}
+
+export default MotorcycleModel;
