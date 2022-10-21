ffb60d12d17c18727f32c43dcd48cc610945c0ea
Guilherme Fernandes <gf_engquimica@hotmail.com>
2022-10-21 14:56:40 -0300

    feat: criação da interface de vehicle (req 2)

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,41 @@
+import { z } from 'zod';
+
+const VehicleZodSchema = z.object({
+  model: z
+    .string({
+      required_error: 'Model is required',
+      invalid_type_error: 'Model must be a string',
+    })
+    .min(3, {
+      message: 'Model must be 3 or more characters long',
+    }),
+  year: z
+    .number({
+      required_error: 'Year is required',
+      invalid_type_error: 'Year must be a number',
+    })
+    .int({ message: 'Must be a integer' })
+    .positive({ message: 'Must be a positive number' })
+    .lte(2022, { message: 'Year must be less than 2022' })
+    .gte(1900, { message: 'Year must be greater than 1900' }),
+  color: z
+    .string({
+      required_error: 'Color is required',
+      invalid_type_error: 'Color must be a string',
+    })
+    .min(3, {
+      message: 'Color must be 3 or more characters long',
+    }),
+  status: z.optional(z.boolean({ invalid_type_error: 'Status must be a boolean' })),
+  buyValue: z
+    .number({
+      required_error: 'BuyValue is required',
+      invalid_type_error: 'BuyValue must be a number',
+    })
+    .int({ message: 'Must be a integer' }),
+});
+
+type IVehicle = z.infer<typeof VehicleZodSchema>;
+
+export { IVehicle, VehicleZodSchema };
+export default IVehicle;
